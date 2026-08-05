28fb006aad4fdc7f5f5a3eb4d698a8d1fb7f9ad5
maazcrafts <maazsabirkhan@gmail.com>
2026-08-05 15:12:43 +0530

    Add responsive mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 //Main portfolio Script
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
